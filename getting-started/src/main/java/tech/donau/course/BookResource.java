a805e0c0c6cadcc48a117d8c0fe7bf9c55eccbac
mph206 <mpharrison206@gmail.com>
2021-03-22 16:18:42 +0000

    Add data sources to getting-started project

--- getting-started/src/main/java/tech/donau/course/BookResource.java
+++ getting-started/src/main/java/tech/donau/course/BookResource.java
@@ -1,21 +1,56 @@
 package tech.donau.course;
 
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.agroal.DataSource;
+import org.checkerframework.common.value.qual.IntRangeFromGTENegativeOne;
 import tech.donau.course.data.Book;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Path("/book")
 public class BookResource {
 
+    @Inject
+    @DataSource("hello")
+    AgroalDataSource dataSource;
+
+    @Inject
+    @DataSource("users")
+    AgroalDataSource usersDataSource;
+
     private static List<Book> books = new ArrayList<>();
 
     static {
         books.add(new Book("Harry Potter 1", "JK Rowling", 1));
+    }
+
+    @GET
+    @Path("/hello")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayHello() throws SQLException {
+        final CallableStatement call = dataSource.getConnection().prepareCall("select * from greeting");
+        call.execute();
+        final ResultSet resultSet = call.getResultSet();
+        resultSet.next();
+        resultSet.next();
+        final String hello = resultSet.getString(2);
+
+        final CallableStatement call2 = usersDataSource.getConnection().prepareCall("select * from user");
+        call2.execute();
+        final ResultSet userSet = call2.getResultSet();
+        userSet.next();
+
+        return hello + " " + userSet.getString(2);
     }
 
     @GET
